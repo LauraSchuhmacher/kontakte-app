1d9710ae8e189f7de76e692ddee64a2bd8298b06
LauraSchuhmacher <l.schuhmacher44@gmail.com>
2025-07-30 14:49:12 +0200

    Add App Icon

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -37,68 +37,72 @@
           </ion-item>
         </div>
       </ion-list>      
-      <ion-modal :is-open="showDetail" @didDismiss="closeDetail()">
-        <ion-header>
-          <ion-toolbar>
-            <ion-buttons slot="start">
-              <ion-button @click="closeDetail()" fill="clear" aria-label="Back">
-                <ion-icon name="arrow-back-outline"></ion-icon>
-              </ion-button>
-              <span
-                style="
-                  vertical-align: middle;
-                  font-size: 1.1em;
-                  font-weight: 500;
-                  margin-left: 8px;
-                "
-              >Kontakt Details</span>
-            </ion-buttons>
-          </ion-toolbar>
-        </ion-header>
-        <ion-content>
-          <div v-if="selectedContact">
-            <p>
-              {{ selectedContact.givenName }} {{ selectedContact.familyName }}
-            </p>
-            <p v-if="selectedContact.phoneNumbers?.length">
-              <ion-icon name="call-outline"></ion-icon>
-              <span
-                style="cursor: pointer; color: var(--ion-color-primary)"
-                @click="callNumber(selectedContact.phoneNumbers[0].value)"
-              >
-                {{ selectedContact.phoneNumbers[0].value }}
-              </span>
-                <ion-button fill="clear" color="medium" @click="copyToClipboard(selectedContact.phoneNumbers[0].value)">
-                  <ion-icon name="copy-outline"></ion-icon>
-                </ion-button>
-            </p>
-            <p v-if="selectedContact.emailAddresses?.length">
-              <ion-icon name="at-outline"></ion-icon>
-              <span
-                style="cursor: pointer; color: var(--ion-color-primary)"
-                @click="writeMail(selectedContact.emailAddresses[0].value)"
-              >
-              {{ selectedContact.emailAddresses[0].value }}
-              </span>
-              <ion-button fill="clear" color="medium" @click="copyToClipboard(selectedContact.emailAddresses[0].value)">
-                  <ion-icon name="copy-outline"></ion-icon>
-                </ion-button>
-            </p>
-            <p v-if="selectedContact.birthday">
-              <ion-icon name="balloon-outline"></ion-icon>
-              {{ selectedContact.birthday.day }}.{{
-                selectedContact.birthday.month
-              }}.{{ selectedContact.birthday.year }}
-            </p>
-          </div>
-          <ion-button @click="confirmAndDeleteContact()">
-            <ion-icon name="trash-outline"></ion-icon>
+     
+     <ion-modal :is-open="showDetail" @didDismiss="closeDetail()">
+  <ion-header>
+    <ion-toolbar>
+      <ion-buttons slot="start">
+        <ion-button @click="closeDetail()" fill="clear" aria-label="Zurück">
+          <ion-icon name="arrow-back-outline"></ion-icon>
+        </ion-button>
+        <ion-title>Kontakt Details</ion-title>
+      </ion-buttons>
+    </ion-toolbar>
+  </ion-header>
+
+  <ion-content class="ion-padding">
+    <ion-card v-if="selectedContact">
+      <ion-card-header>
+        <ion-card-title>
+          {{ selectedContact.givenName }} {{ selectedContact.familyName }}
+        </ion-card-title>
+      </ion-card-header>
+
+      <ion-card-content>
+        <ion-item v-if="selectedContact.phoneNumbers?.length">
+          <ion-icon slot="start" name="call-outline"></ion-icon>
+          <ion-label @click="callNumber(selectedContact.phoneNumbers[0].value)" style="cursor: pointer; color: var(--ion-color-primary)">
+            {{ selectedContact.phoneNumbers[0].value }}
+          </ion-label>
+          <ion-button slot="end" fill="clear" color="medium" @click="copyToClipboard(selectedContact.phoneNumbers[0].value)">
+            <ion-icon name="copy-outline"></ion-icon>
           </ion-button>
-          <ion-button @click="updateContact()">
-            <ion-icon name="create-outline"></ion-icon>
+        </ion-item>
+
+        <ion-item v-if="selectedContact.emailAddresses?.length">
+          <ion-icon slot="start" name="at-outline"></ion-icon>
+          <ion-label @click="writeMail(selectedContact.emailAddresses[0].value)" style="cursor: pointer; color: var(--ion-color-primary)">
+            {{ selectedContact.emailAddresses[0].value }}
+          </ion-label>
+          <ion-button slot="end" fill="clear" color="medium" @click="copyToClipboard(selectedContact.emailAddresses[0].value)">
+            <ion-icon name="copy-outline"></ion-icon>
           </ion-button>
-        </ion-content>
-      </ion-modal>
+        </ion-item>
+
+        <ion-item v-if="selectedContact.birthday">
+          <ion-icon slot="start" name="balloon-outline"></ion-icon>
+          <ion-label>
+            {{ selectedContact.birthday.day }}.{{ selectedContact.birthday.month }}.{{ selectedContact.birthday.year }}
+          </ion-label>
+        </ion-item>
+      </ion-card-content>
+    </ion-card>
+
+    <div class="ion-text-center">
+      <ion-button color="danger" @click="confirmAndDeleteContact()">
+        <ion-icon slot="start" name="trash-outline"></ion-icon>
+        Löschen
+      </ion-button>
+
+      <ion-button color="primary" @click="updateContact()">
+        <ion-icon slot="start" name="create-outline"></ion-icon>
+        Bearbeiten
+      </ion-button>
+    </div>
+  </ion-content>
+</ion-modal>
+
+
 
       <ion-modal :is-open="showCreateModal" @didDismiss="closeCreateModal">
         <ion-header>
